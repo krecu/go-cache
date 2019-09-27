9bd77983ef58cd2bd95df6e48b539c941b076a9e
Kretsu Evgeniy <krecu.me@ya.ru>
2019-09-27 17:32:23 +0300

    fix test

--- go-cache/cache_test.go
+++ go-cache/cache_test.go
@@ -64,39 +64,6 @@ var bigValue = &TestItem{
 			`,
 }
 
-func TestCache_SetGetO(t *testing.T) {
-	var (
-		key  = "test"
-		err  error
-		item *TestItem
-	)
-
-	proto, _ := New(Option{
-		Evicted:  10,
-		Compress: true,
-		Expire:   10,
-		Flush:    10,
-	})
-
-	err = proto.SetO(key, TestItem{
-		Id:   "1",
-		Body: "1",
-	})
-	if err != nil {
-		t.Errorf("Err: %s", err)
-	}
-
-	item = &TestItem{}
-	err = proto.GetO(key, &item)
-	if err != nil {
-		t.Error(err)
-	} else {
-		if item.Id != "1" || item.Body != "1" {
-			t.Error("no equal")
-		}
-	}
-}
-
 func TestCache_SetGet(t *testing.T) {
 
 	var (
